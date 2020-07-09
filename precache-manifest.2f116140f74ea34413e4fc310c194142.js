898a041200853cce8962910a3b07fccdfe29f25c
nazmeln <nazik100378@gmail.com>
2020-07-09 14:10:14 +0300

    Updates

---
+++ precache-manifest.2f116140f74ea34413e4fc310c194142.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358031a3b0b7ba1888a97e4ab3f93796",
+    "url": "./index.html"
+  },
+  {
+    "revision": "175b87f73b7407f3b9b7",
+    "url": "./static/css/main.3d77dcb3.chunk.css"
+  },
+  {
+    "revision": "8d2d2f90b46b24c4710e",
+    "url": "./static/js/2.cc74ebc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.cc74ebc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "175b87f73b7407f3b9b7",
+    "url": "./static/js/main.2de3c06d.chunk.js"
+  },
+  {
+    "revision": "35878c0637cea8221062",
+    "url": "./static/js/runtime-main.ba67aef9.js"
+  }
+]);
